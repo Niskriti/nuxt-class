de1271b775e5eb0b1fa17c9f473081df17efc994
Niskriti <niskritishrestha@gmail.com>
2023-06-11 02:28:28 -0700

    hello and card created

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,62 @@
 <template>
-    <div>
-        <h1>This is home page</h1>
-    </div>
+<div>
+    <section class="hello py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Welcome to the home page</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis nostrum repellendus, porro aliquam illo molestiae. Laboriosam corrupti quod, mollitia vero, nihil non omnis eveniet nobis id cum itaque voluptatibus iste ut odit, doloribus sed iure dolorum deleniti fugiat? Inventore est quod iusto mollitia dolore eaque consectetur quae sed sequi saepe?</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- card -->
+    <section class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2023/05/30/16/50/milk-8029140_640.png" alt="">
+                        <h1>This is something</h1>
+
+                    </div>
+
+                </div>
+                <div class="col-md-8">
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="https://cdn.pixabay.com/photo/2023/05/28/05/34/bird-8022869_640.jpg" alt="">
+                      
+                    </div>
+                    <div class="col-md-6">
+                        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita pariatur vitae exercitationem, tenetur illum delectus nulla eius consectetur neque magni!</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+            </div>
+        </div>
+
+    </section>
+</div>
+
 </template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.hello {
+    background-color: grey;
+    text-align: center;
+
+}
+
+.card {
+    box-radius: 4px;
+    box-shadow: 10px 10px 10px 10px grey;
+}
+</style>
