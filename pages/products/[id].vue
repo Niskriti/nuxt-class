1205291656aa0fb6912fea518502b0a485f33456
Niskriti <niskritishrestha@gmail.com>
2023-06-13 02:20:12 -0700

    id created

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    <h1>Product Detailed page</h1>
+    <h2>The product number is {{ route.params.id }}</h2>
+</div>
+</template>
+
+<script setup>
+     const route = useRoute();
+</script>
+
+<style lang="scss" scoped>
+
+</style>
