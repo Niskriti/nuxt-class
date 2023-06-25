b202dc76c7302ce765e50da2b6170e675595890b
Niskriti <niskritishrestha@gmail.com>
2023-06-25 02:18:23 -0700

    product created

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,14 +1,41 @@
 <template>
 <div>
-    <h1>Product Detailed page</h1>
-    <h2>The product number is {{ route.params.id }}</h2>
+    <div class="container">
+        <div class="row">
+         <div class="col-md-4">
+            <img :src="product.images[0]" alt="" class="img-fluid">
+         </div>
+         <div class="col-md-8">
+            <div>
+                <h1>{{ product.title }}</h1>
+            </div>
+            <div>
+                <p>{{ product.description }}</p>
+            </div>
+            <div>
+                <p>{{ product.price }}</p>
+            </div>
+            <div>
+                <h6>category</h6>
+                <p>{{ product.category.name }}</p>
+                <img :src="product.category.image" alt="" width="80" class="rounded">
+            </div>
+            <div>
+                <span><i class="fa-solid fa-circle-minus"></i></span>
+                <span class="fs-1 px-5">1</span>
+                <span><i class="fa-solid fa-circle-plus"></i></span>
+            </div>
+         </div>
+        </div>
+    </div>
 </div>
 </template>
 
 <script setup>
      const route = useRoute();
+     const {data:product} = await useFetch(`https://api.escuelajs.co/api/v1/products/${route.params.id}`)
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,21 +1,24 @@
 <template>
-    <div>
-        <section class="py-5">
+<div>
+
+    <section class="py-5">
         <div class="container">
-            <div class="row">
-                <div class="col-md-3" v-for="(p,index) in data.data._rawValue" :key="index">
-                  <productCard :product="p"/>
-                 
+            <div class="row g-4">
+                <div class="col-md-3" v-for="(p,index) in product" :key="index">
+                    <nuxt-link :to="`/products/${p.id}`">
+                        <productCard :product="p" />
+                    </nuxt-link>
                 </div>
             </div>
-      </div>
+        </div>
     </section>
-    </div>
+</div>
 </template>
 
 <script setup>
-      const data = await useFetch("https://api.escuelajs.co/api/v1/products");
-      console.warn(data.data._rawValue)
+const {
+    data: product
+} = await useFetch("https://api.escuelajs.co/api/v1/products");
 </script>
 
 <style  scoped>
